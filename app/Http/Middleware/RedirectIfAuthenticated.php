5943dc1b7768548621166e49b584e4e16cb7d54e
mustofa <buffonmustofa@gmail.com>
2019-06-04 20:14:03 +0700

    change redirect after login

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -18,7 +18,7 @@ class RedirectIfAuthenticated
     public function handle($request, Closure $next, $guard = null)
     {
         if (Auth::guard($guard)->check()) {
-            return redirect('/airlines/AWQ/T2');
+            return redirect('/airlines/AWQ/T2/D');
         }
 
         return $next($request);
